fedf6fc093b3cac97e08d6eee6749e1597b2ca00
Ryan Brady <ryanbrady369@gmail.com>
2018-10-31 22:15:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ let newID = 1000000000 + oldID
 
 let ageIsValid = Number.isInteger(currentAge)
 
-let randomNumber = Math.random() * 20 + 1
+let randomNumber = Math.random() * 20
 
 let randomInteger = randomNumber.floor()
